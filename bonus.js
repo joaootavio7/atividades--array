a6885d5d07f1d3e3103d0c8f51f92831becc0450
João Otávo <ojoao21513@gmail.com>
2025-05-29 16:50:39 -0300

    atividades

---
+++ bonus.js
@@ -0,0 +1,17 @@
+console.clear();
+var teclado = require("Prompt-sync")();
+var numeros = [];
+var temporario = 0;
+for (var x = 0; x <= 9; x++) {
+    numeros[x] = parseInt(teclado("Digite o n\u00FAmero do \u00EDndice ".concat(x, " do Array: ")));
+}
+for (var z = 0; z < 10; z++) {
+    for (var y = z + 1; y < 10; y++) {
+        if (numeros[z] < numeros[y]) {
+            temporario = numeros[z];
+            numeros[z] = numeros[y];
+            numeros[y] = temporario;
+        }
+    }
+}
+console.log("".concat(numeros));
---
+++ bonus.ts
@@ -0,0 +1,20 @@
+console.clear()
+let teclado = require(`Prompt-sync`)()
+let numeros: number[] = []
+let temporario: number = 0
+
+for (let x = 0; x <= 9; x++) {
+    numeros[x] = parseInt(teclado(`Digite o número do índice ${x} do Array: `))
+}
+
+for (let z = 0; z < 10; z++) {
+
+    for (let y=z+1; y < 10; y++) {
+        if (numeros[z] < numeros[y]) {
+            temporario = numeros[z]
+            numeros[z] = numeros[y]
+            numeros[y] = temporario
+        }
+    }
+}
+console.log(`${numeros}`)
